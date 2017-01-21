496d79d97b37d7255d15440f19a8891266f23472
Frank Altamirano <smurfy@bigpond.net.au>
2017-01-21 15:40:17 +1100

    Added Main App component function

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// Create a new component. Component contains the HTML template.
+
+const App = () => <div><h1>Hello World</h1></div>;
+
+// Tell this components HTML template and add it to the DOM.

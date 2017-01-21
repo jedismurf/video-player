136a10534550b8641293c4b011e2305064f7342d
Frank Altamirano <smurfy@bigpond.net.au>
2017-01-21 16:28:06 +1100

    Made main component functional

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create a new component. Component contains the HTML template.
 
-const App = () => <div><h1>Hello World</h1></div>;
+const App = () => <div><h1>Hello World</h1></div>
 
 // Tell this components HTML template and add it to the DOM.
+ReactDOM.render(<App />, document.querySelector(".container"));

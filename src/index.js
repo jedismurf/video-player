2d4e93286271377c96b051d821d6a2df1fb9a155
Frank Altamirano <smurfy@bigpond.net.au>
2017-01-22 09:22:52 +1100

    Added code to video list, and added video list to main app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YouTubeSearch from 'youtube-api-search';
 import SearchBar from  './components/search_bar';
+import VideoList from './components/video_list';
 import { GOOGLE_API_KEY } from './api/google_api';
 
 // Create a new component. Component contains the HTML template.
@@ -25,6 +26,9 @@ class App extends Component {
             <div>
                 <h1>Hello World</h1>
                 <SearchBar />
+                <div className="col-md-8">
+                </div>
+                <VideoList />
             </div> 
         );
     }

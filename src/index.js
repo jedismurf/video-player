1ad0e918421988afcf30dc74f16656ce760e1dd9
Frank Altamirano <smurfy@bigpond.net.au>
2017-01-21 17:58:48 +1100

    Added code to search bar component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YouTubeAPISearch from 'youtube-api-search';
+import SearchBar from  './components/search_bar';
+import GOOGLE_API_KEY from './api/google_api';
 
 // Create a new component. Component contains the HTML template.
 
-const App = () => <div><h1>Hello World</h1></div>
+const App = () => { 
+    return(
+        <div>
+            <h1>Hello World</h1>
+            <SearchBar />
+        </div>
+    ); 
+};
 
 // Tell this components HTML template and add it to the DOM.
 ReactDOM.render(<App />, document.querySelector(".container"));
